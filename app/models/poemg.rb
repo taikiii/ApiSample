3efe24299076dd9ba5969fd5ecd02da6744cc987
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-07 15:56:16 +0900

    iiii

---
+++ app/models/poemg.rb
@@ -0,0 +1,2 @@
+class Poemg < ApplicationRecord
+end
---
+++ db/migrate/20170507022259_create_poemgs.rb
@@ -0,0 +1,10 @@
+class CreatePoemgs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :poemgs do |t|
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
