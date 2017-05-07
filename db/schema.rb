942626372f2df59f69413b0221b43b04b993fa9e
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-07 17:06:53 +0900

    uuuuuu

--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(version: 20170507073658) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "poemgs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "poems", force: :cascade do |t|
     t.string "title"
     t.text "content"
