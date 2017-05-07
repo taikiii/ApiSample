9f3a9632372bcb16f3365ebd387eca3ab850dce3
Taiki Kondo <cbu_hh@yahoo.co.jp>
2017-05-07 16:41:28 +0900

    kkakkakaka

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507022259) do
+ActiveRecord::Schema.define(version: 20170507073658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20170507022259) do
   end
 
   create_table "poemgs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "poems", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
